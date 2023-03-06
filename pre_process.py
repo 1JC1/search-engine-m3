7a31c29b0b301f4e98140297eeefa91e32f37d44
Enna Phan <ennap@uci.edu>
2023-03-06 09:51:40 -0800

    fixing tf-idf weighting

--- pre_process.py
+++ pre_process.py
@@ -12,19 +12,19 @@ def calc_tf_idf(total_doc_count: int, df: int, tf: int):
     return tf * idf 
 
 # note: have to account for repeating tokens (ex. ics ics)
-def compare_tf_idf(query_tokens: list[str], total_doc_count: int, search_num, intersects: list['Posting']):
+def compare_tf_idf(query_tokens: list[str], total_doc_count: int, search_num):
     scores = defaultdict(int)
-    length = dict()
     
     for token in query_tokens:
-        query_weight = calc_tf_idf(total_doc_count, len(postings), 1 + log(query_tokens.count(token)))
+        token_postings = main_index[token]
+        query_weight = calc_tf_idf(total_doc_count, len(token_postings), 1 + log(query_tokens.count(token)))
         
-        for posting in intersects:
-            doc_weight = calc_tf_idf(total_doc_count, len(intersects), posting.get_tf()) 
+        for posting in token_postings:
+            doc_weight = calc_tf_idf(total_doc_count, len(token_postings), posting.get_tf()) 
             # replace get_tf with correct method name; maybe change to just tf and cosine normalization
             scores[posting.get_docID()] += query_weight * doc_weight
             
     for docID in scores:
-        scores[docID] = scores[docID] / length[docID] # change once we confirm what length is
+        scores[docID] = scores[docID] # / length[docID] # change once we confirm what length is
     
     return scores.values().sort(scores[1])[0:search_num] # assuming we want smaller angles
