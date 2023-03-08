806112fda15d1f4cc5276dde4b3e7191da184e53
Enna Phan <ennap@uci.edu>
2023-03-08 14:46:35 -0800

    condense loops and use union docs

--- pre_process.py
+++ pre_process.py
@@ -6,30 +6,26 @@ from collections import defaultdict
 # instead of comparing tf-idf for any doc containing at least one query term, should set separate
 # function to find docs that contain more than one (see lecture 22)
 
-def compare_tf_idf(query: str, total_doc_count: int):
+def compare_tf_idf(query: str, union_docs: list('Postings'), doc_vec_sizes: dict(), total_doc_count: int, search_num: int):
     query_tokens = set(query.split())
     
     query_weights = defaultdict(int)
     scores = defaultdict(int)
     
     for token in query_tokens:
-        token_postings = main_index[token] # change to file seeking
-        
         q_tf_wt = 1 + log(query.count(token), base = 10)
-        q_idf = log((total_doc_count / len(token_postings)), base = 10)
+        q_idf = log((total_doc_count / len(main_index[token])), base = 10) # change to file seeking
         
         query_wt = q_tf_wt * q_idf
         query_weights[token] = query_wt
         
-    query_vector_size = sqrt(sum([w**2 for w in query_weights])) # used in cosine normalization
+        for posting in union_docs:
+            scores[posting.get_docID()] = query_wt * posting.get_tf()
+             
+    query_vec_size = sqrt(sum([w**2 for w in query_weights])) # used in cosine normalization
     
-    for token in query_weights:
-        query_weights[token] = query_weights[token] / query_vector_size
     
-    for token in query_tokens:
-        for posting in main_index[token]:
-            doc_weight = posting.get_tf() / doc_vector_size # replace with actual way to get document vector size
-            scores[posting.get_docID()] += query_weights[token] * doc_weight
-        
+    for docID in scores:
+        scores[docID] = scores[docID] / (query_vec_size * doc_vec_sizes[docID])
     
     return scores.values().sort(scores[1], reverse = True)[0:search_num]
